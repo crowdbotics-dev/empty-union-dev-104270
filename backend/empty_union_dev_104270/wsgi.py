9bc25f0041e6a6051d0d1387c6138f8c48581749
Crowdbotics <engineering@crowdbotics.com>
2023-07-26 07:56:17 +0000

    Initial commit.

---
+++ backend/empty_union_dev_104270/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for empty_union_dev_104270 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'empty_union_dev_104270.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "empty-union-dev-104270",
+  url: "https://empty-union-dev-104270.botics.co",
+  api: "https://empty-union-dev-104270.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
